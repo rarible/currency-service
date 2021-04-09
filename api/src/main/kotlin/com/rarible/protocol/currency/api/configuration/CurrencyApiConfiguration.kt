4270f7a60ce2dafeebc753244e6c52cb301c3ac8
Andrey Atapin <a.atapin@gmail.com>
2021-04-09 13:44:13 +0500

    Configuration properties fix

--- api/src/main/kotlin/com/rarible/protocol/currency/api/configuration/CurrencyApiConfiguration.kt
+++ api/src/main/kotlin/com/rarible/protocol/currency/api/configuration/CurrencyApiConfiguration.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.rarible.protocol.currency.api.gecko.GeckoApi
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.roborox.reactive.feign.FeignHelper
@@ -11,6 +12,7 @@ import ru.roborox.reactive.persist.configuration.EnableRoboroxMongo
 
 @Configuration
 @EnableRoboroxMongo
+@EnableConfigurationProperties(CurrencyApiProperties::class)
 class CurrencyApiConfiguration(
     val properties: CurrencyApiProperties
 ) {
