a2ffad627f7596c71ddec777cefc620dd0966aef
Andrey Atapin <a.atapin@gmail.com>
2021-04-30 14:19:09 +0500

    Better logging

--- api/src/main/kotlin/com/rarible/protocol/currency/api/controller/CurrencyController.kt
+++ api/src/main/kotlin/com/rarible/protocol/currency/api/controller/CurrencyController.kt
@@ -7,6 +7,8 @@ import com.rarible.protocol.currency.api.CurrencyApi
 import com.rarible.protocol.currency.core.configuration.CurrencyApiProperties
 import com.rarible.protocol.currency.api.dto.RateDto
 import com.rarible.protocol.currency.core.repository.RateRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.core.convert.ConversionService
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
@@ -18,16 +20,30 @@ class CurrencyController(
     private val rateRepository: RateRepository,
     private val conversionService: ConversionService,
     private val currencyApiProperties: CurrencyApiProperties
-): CurrencyApi {
+) : CurrencyApi {
 
-    override fun getRate(blockchain: Blockchain, address: Address, at: Date): Mono<RateDto> = coroutineToMono {
-        currencyApiProperties
-            .byAddress(blockchain, address)
-            ?.let {
-                rateRepository.getRate(it, at)
+    val logger: Logger = LoggerFactory.getLogger(CurrencyController::class.java)
+
+    override fun getRate(blockchain: Blockchain, address: Address, at: Date): Mono<RateDto> {
+        logger.info("Get rate for [{}/{}] at {}", blockchain, address, at)
+        val coinId = currencyApiProperties.byAddress(blockchain, address)
+
+        return coroutineToMono {
+            if(coinId == null) {
+                logger.warn(
+                    "Coin [{}/{}] is not supported. If this coin should be tracked, add it to application.yml.",
+                    blockchain,
+                    address
+                )
+                null
+            } else {
+                logger.info("Coin id = {}", coinId)
+                val geckoRate = rateRepository.getRate(coinId, at)
+                logger.info("Gecko response: {}", geckoRate)
+                geckoRate?.let {
+                    conversionService.convert<RateDto>(it)
+                }
             }
-            ?.let {
-                conversionService.convert<RateDto>(it)
-            }
+        }
     }
 }
