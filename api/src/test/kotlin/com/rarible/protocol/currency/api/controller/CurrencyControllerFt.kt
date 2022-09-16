2cae86d5aee72961546b8c102c7dec71aa2e82c8
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2022-09-16 11:16:47 +0400

    fix: All currencies for Immutablex (#24)

--- api/src/test/kotlin/com/rarible/protocol/currency/api/controller/CurrencyControllerFt.kt
+++ api/src/test/kotlin/com/rarible/protocol/currency/api/controller/CurrencyControllerFt.kt
@@ -195,13 +195,16 @@ internal class CurrencyControllerFt(
         val currencies = client.allCurrencies?.awaitFirstOrNull()?.currencies!!
 
         val wethCurrencies = currencies.filter { it.currencyId == "weth" }
-        assertThat(wethCurrencies).hasSize(2)
+        assertThat(wethCurrencies).hasSize(3)
 
         val eth = wethCurrencies.find { it.blockchain == BlockchainDto.ETHEREUM }!!
+        // Should be the same as for Eth despite absence in configuration
+        val imx = wethCurrencies.find { it.blockchain == BlockchainDto.IMMUTABLEX }!!
         val poly = wethCurrencies.find { it.blockchain == BlockchainDto.POLYGON }!!
 
         assertThat(eth.address).isEqualTo("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
         assertThat(poly.address).isEqualTo("0x7ceb23fd6bc0add59e62ac25578270cff1b9f619")
+        assertThat(imx.address).isEqualTo(eth.address)
     }
 
     @Test
