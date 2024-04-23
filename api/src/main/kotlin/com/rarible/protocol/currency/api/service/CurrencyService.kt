3a8af334180c9652ae62fea4e37f4f84a15d9a35
Artem Yakischik <a.yakischik@rarible.com>
2024-04-23 18:49:29 +0400

    Urgent patch for lightlink rate

--- api/src/main/kotlin/com/rarible/protocol/currency/api/service/CurrencyService.kt
+++ api/src/main/kotlin/com/rarible/protocol/currency/api/service/CurrencyService.kt
@@ -2,8 +2,10 @@ package com.rarible.protocol.currency.api.service
 
 import com.rarible.protocol.currency.core.model.Rate
 import com.rarible.protocol.currency.core.repository.RateRepository
+import org.bson.types.ObjectId
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.math.BigDecimal
 import java.time.Instant
 
 @Component
@@ -14,7 +16,25 @@ class CurrencyService(
     private val logger = LoggerFactory.getLogger(CurrencyService::class.java)
 
     suspend fun getRate(coinId: String, at: Instant?): Rate? {
+        getHardcodedRate(coinId)?.let {
+            return Rate(
+                id = ObjectId(),
+                currencyId = coinId,
+                date = Instant.now(),
+                rate = it,
+                version = null
+            )
+        }
         logger.info("Coin id = {}", coinId)
         return at?.let { rateRepository.getRate(coinId, at) } ?: rateRepository.findLast(coinId)
+    }
+
+    // TODO !!!
+    // There is no rates for these currencies at CoinGecko, we need to fetch them from somewhere else
+    private fun getHardcodedRate(coinId: String): BigDecimal? {
+        return when (coinId) {
+            "lightlink" -> BigDecimal("0.2811")
+            else -> null
+        }
     }
 }
